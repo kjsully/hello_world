d58619175fb69a99e974d599933eb2658ccc85c1
Kyle Sullivan <kylesullivan123@gmail.com>
2021-06-04 14:44:10 -0700

    commit

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+print('Hello World')
+name = 'Kyle'
+print('Hello, ' + name)
+num = 22
+print('Hello, ' + str(num))
+food1 = 'peanut butter'
+food2 = 'teriyaki'
+print('I love to eat ' + food1 + ' and ' + food2)
+print(f"I love to eat {food1} and {food2}")
